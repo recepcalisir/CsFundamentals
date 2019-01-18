12bc0d169968bfa12ebbf6bc93ebf9774d5b7442
Recep Çalışır <recepcalisir92@gmail.com>
2019-01-18 10:30:33 +0200

    Added Core Files via Upload

---
+++ CsFundamentals/CsFundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
